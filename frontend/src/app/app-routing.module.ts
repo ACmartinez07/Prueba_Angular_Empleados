daa8467bf703b886d6b57e15fa9921a6afef4079
Andres Martinez <andrescamm07@gmail.com>
2021-11-05 23:02:22 -0500

    creadas las rutas y el navbar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { CrearComponent } from './componentes/crear/crear.component';
+import { ActualizarComponent } from './componentes/actualizar/actualizar.component';
+import { EmpleadoComponent } from './componentes/empleado/empleado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'crear', component: CrearComponent },
+  { path: 'actualizar', component: ActualizarComponent },
+  { path: 'empleado/:id', component: EmpleadoComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'inicio' },
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
